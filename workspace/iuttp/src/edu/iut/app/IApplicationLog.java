fa2b87bfc2febfa7e5821bcce1e4d560a41f86aa
unknown <user.name>
2015-11-15 23:26:26 +0100

    tp1

--- workspace/iuttp/src/edu/iut/app/IApplicationLog.java
+++ workspace/iuttp/src/edu/iut/app/IApplicationLog.java
@@ -1,10 +1,8 @@
 package edu.iut.app;
 
 public interface IApplicationLog {
-	/** TP1 : Créérr les méthodes :
-	 - setMessage
-	  - getMessage
-	  - addListener
-	  - getApplicationLogListeners();
-	*/
+	public void setMessage(String message);
+	public String getMessage();
+	public void addListener(IApplicationLogListener listener);
+	public IApplicationLogListener[] getApplicationLogListeners();
 }
